ac90a003bd086d69418bf20bc37e83c2ae386f82
Joseph <mwanikij118@gmail.com>
2025-06-23 21:49:57 +0300

    Adds index.js

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+   const seenNumbers = {};
+
+  for (const number of array) {
+    // n steps
+    const complement = target - number;
+    if (complement in seenNumbers) return true;
+    seenNumbers[number] = true;
+  }
+
+  return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
+
 */
+function findSock(array) {
+  for (const item of array) {
+    if (item === "sock") return "sock";
+  }
+}
 
 /* 
   Add your pseudocode here
 */
+
 
 /*
   Add written explanation of your solution here
@@ -29,6 +46,16 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([4], 4));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([-1, 2, 7, 4], 6));
 }
 
 module.exports = hasTargetSum;
